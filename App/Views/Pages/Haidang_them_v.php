7b3bbe8f83b9e6993f82605990903bf3abf30e58
longdii <duylonghiha@gmail.com>
2024-07-09 00:02:56 +0700

    Update Haidang_them_v.php

--- App/Views/Pages/Haidang_them_v.php
+++ App/Views/Pages/Haidang_them_v.php
@@ -21,7 +21,7 @@
 </head>
 
 <body>
-    <form action="/Haidang_them/themmoi" method="post">
+    <form action="/WebKhuVuiChoi/Haidang_them/themmoi" method="post">
         <div class="header">
             <ul class="header-list">
                 <li class="header-item">
