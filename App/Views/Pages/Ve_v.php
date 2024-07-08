2797406de382182879378aeef4951ba776145460
longdii <duylonghiha@gmail.com>
2024-07-09 00:25:58 +0700

    Update Ve_v.php

--- App/Views/Pages/Ve_v.php
+++ App/Views/Pages/Ve_v.php
@@ -21,7 +21,7 @@
 </head>
 
 <body>
-    <form action="/Ve_c/timkiem" method="post">
+    <form action="/WebKhuVuiChoi/Ve_c/timkiem" method="post">
         <div class="header">
             <ul class="header-list">
                 <li class="header-item">
