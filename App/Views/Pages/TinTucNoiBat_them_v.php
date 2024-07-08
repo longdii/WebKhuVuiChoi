d5df937074c822111db64bd2d78d3f28a74f2653
longdii <duylonghiha@gmail.com>
2024-07-09 00:13:20 +0700

    Update TinTucNoiBat_them_v.php

--- App/Views/Pages/TinTucNoiBat_them_v.php
+++ App/Views/Pages/TinTucNoiBat_them_v.php
@@ -21,7 +21,7 @@
 </head>
 
 <body>
-    <form action="/TinTuc_them/themmoi" method="post">
+    <form action="/WebKhuVuiChoi/TinTuc_them/themmoi" method="post">
         <div class="header">
             <ul class="header-list">
                 <li class="header-item">
