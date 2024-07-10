b6ee6c24a5c90eceaa06c68c27f852fd8a0a9e12
longdii <duylonghiha@gmail.com>
2024-07-10 15:50:01 +0700

    Update DangNhap_v.php

--- App/Views/Pages/DangNhap_v.php
+++ App/Views/Pages/DangNhap_v.php
@@ -10,7 +10,7 @@
     <title>Đăng Nhập</title>
 </head>
 <body>
-    <form method="post" action="http://khudulichSunWorld.com/DangNhap/KiemTraDangNhap">
+    <form method="post" action="var/www/WebKhuVuiChoi/DangNhap/KiemTraDangNhap">
     <div class="noidung">
         <div class="bang">
             <div class="anh"></div>
